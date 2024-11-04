7188469ddef758e8e21a2f2456ca9932278c725f
Engineerviju <157585634+Engineerviju@users.noreply.github.com>
2024-11-04 18:57:30 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,8 +1,10 @@
 <?php
-$data=file_get_contents('php://input');
-$logFile="webhooksentdata.json";
-$log=fopen($logFile, "a");
-fwrite($log, $data);
-fclose($log);
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405); // Respond with 405 Method Not Allowed
+    exit("Method Not Allowed");
+}
 
+$data = file_get_contents('php://input');
+$logFile = "webhooksentdata.json";
+file_put_contents($logFile, $data, FILE_APPEND);
 ?>
